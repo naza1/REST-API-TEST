956d595118e69cb32aee441baf663442c720bf0e
nreal <nreal@makingsense.com>
2016-07-28 15:02:11 -0300

    Update project

---
+++ Tests/GoogleTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HypermediaApiTests.Util;
+using System.Net.Http;
+using System.Net;
+
+namespace HypermediaApiTests.Tests
+{
+    [TestClass]
+    public class Test
+    {
+
+        [TestMethod]
+        public void GoogleTest()
+        {
+            var response = Helper.CreateRequest("?p=test", HttpMethod.Get);
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+        }
+    }
+}
